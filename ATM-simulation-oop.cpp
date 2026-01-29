179cf2f296e621b98d148b28ba9a0d847dd7aa1a
Hanan Qaisar <hananqaisar316@gmail.com>
2026-01-29 20:16:40 +0500

    Basic ATM Management

---
+++ ATM-simulation-oop.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class BankAccount
+{
+    int pin;
+    int accNo;
+    string name;
+    int balance;
+    int chance;
+
+public:
+    BankAccount(int account, string name, int balance, int pin, int chance)
+    {
+        accNo = account;
+        this->name = name;
+        this->balance = balance;
+        this->pin = pin;
+        this->chance = chance;
+    }
+    void deposit(int deposit);
+    void withdraw(int withdraw);
+    void check();
+    void display();
+    void choice(int n);
+    bool checkPin();
+};
+void BankAccount ::deposit(int deposit)
+{
+
+    if (deposit <= 0)
+    {
+        cout << "Sorry! This amount is not deposited" << endl;
+    }
+    else
+    {
+        balance += deposit;
+        cout << "You deposit " << deposit << " rs sucessfully" << endl;
+    }
+}
+
+void BankAccount ::withdraw(int withdraw)
+{
+    if (withdraw > balance)
+    {
+        cout << "Sorry! " << withdraw << " amount is not in your account" << endl;
+    }
+    else
+    {
+        cout << withdraw << " rs is withdraw sucessfully" << endl;
+        balance -= withdraw;
+    }
+}
+void BankAccount ::check()
+{
+
+    char ch;
+    cout << "Want to check account balance?((y or n): )" << endl;
+    cin >> ch;
+    if (ch == 'Y' || ch == 'y')
+    {
+        cout << "Your balance is :" << balance << endl;
+    }
+    else if (ch == 'N' || ch == 'n')
+    {
+        cout << "Thanks for using ATM" << endl;
+    }
+    else
+        cout << "Enter valid choice:" << endl;
+}
+void BankAccount ::display()
+{
+    cout << "Account Owner: " << name << endl;
+    cout << "Account id " << accNo << endl;
+    cout << "Balance: " << balance << endl;
+}
+bool BankAccount ::checkPin()
+{
+    int enteredpin;
+
+    cout << "Enter pin: ";
+    while (chance > 0)
+    {
+        cin >> enteredpin;
+        if (enteredpin == pin)
+        {
+            cout << "Correct pin" << endl;
+            return true;
+        }
+        else
+        {
+            cout << "Wrong pin " << --chance << " more chance(s) left" << " otherwise permission will be denied\nTry again: " << endl;
+        }
+    }
+    cout << "Sorry permission denied\n";
+    return false;
+}
+void BankAccount ::choice(int n)
+{
+    int depositAmount;
+    int withdrawAmount;
+    if (!checkPin())
+        return;
+    else
+    {
+        switch (n)
+        {
+        case 1:
+        {
+            cout << "Enter amount to deposit: ";
+            cin >> depositAmount;
+            deposit(depositAmount);
+        }
+        break;
+
+        case 2:
+        {
+            cout << "Enter amount to Withdraw: ";
+            cin >> withdrawAmount;
+            withdraw(withdrawAmount);
+        }
+        break;
+
+        case 3:
+            check();
+            break;
+
+        case 4:
+            display();
+            break;
+
+        case 5:
+            cout << "logout sucessfull" << endl;
+            break;
+
+        default:
+            cout << "Enter valid choice" << endl;
+        }
+    }
+}
+int main()
+{
+    int choice;
+    BankAccount account1(1199, "Hanan Qaisar", 0, 1493, 3);
+    cout << "\n====ATM DISPLAY====\n";
+    cout << "Choose one option:" << endl
+         << "1. Deposit" << endl
+         << "2. Withdraw" << endl
+         << "3. Check Balabnce" << endl
+         << "4. Display detail" << endl
+         << "5. End" << endl;
+
+    do
+    {
+        cin >> choice;
+        if (choice < 0 || choice > 5)
+        {
+            cout << "Enter valid Choice" << endl;
+        }
+        else
+        {
+            account1.choice(choice);
+            if (choice != 5)
+            {
+                cout << "Choose next option:" << endl;
+            }
+        }
+    } while (choice != 5);
+
+    return 0;
+}
